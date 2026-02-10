6e5a082c47db7d0d3a58cbc5ded85ed3c647ab56
Khadegaali <125767106+Khadegaali@users.noreply.github.com>
2026-02-10 14:32:29 +0200

    security.py

--- app/core/security.py
+++ app/core/security.py
@@ -2,21 +2,18 @@ from passlib.context import CryptContext
 from datetime import datetime, timedelta
 from jose import jwt
 
-SECRET_KEY = "YOUR_SECRET_KEY_HERE"  # غيري ده لمفتاحك السري
+SECRET_KEY = "YOUR_SECRET_KEY_HERE" 
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 pwd_context = CryptContext(schemes=["bcrypt_sha256"], deprecated="auto")
 
-# تشفير كلمة السر
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
 
-# التحقق من كلمة السر
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
-# إنشاء JWT token
 def create_access_token(data: dict, expires_delta: int = ACCESS_TOKEN_EXPIRE_MINUTES):
     to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(minutes=expires_delta)
