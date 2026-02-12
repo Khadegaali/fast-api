1eb70e7afce47f14e664214ac54f28d877fd4d14
Khadegaali <125767106+Khadegaali@users.noreply.github.com>
2026-02-12 14:33:32 +0200

    Update item_model.py

--- app/tables/models/item_model.py
+++ app/tables/models/item_model.py
@@ -8,4 +8,4 @@ class Item(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
 
-    user_items = relationship("UserItem", back_populates="item")  # لازم يكون في UserItem عمود item_id معرف ForeignKey("items.id")
+    user_items = relationship("UserItem", back_populates="item") 
